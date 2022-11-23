beb4b2207b380e434d1ef1361a7abfdfbe56d2bb
Vikram Watkar <103982849+vikramwatkar96@users.noreply.github.com>
2022-11-23 17:21:59 +0530

    Second Commit

--- src/main/java/com/vikram/Services/DatabaseServices.java
+++ src/main/java/com/vikram/Services/DatabaseServices.java
@@ -3,9 +3,12 @@ package com.vikram.Services;
 import java.util.List;
 
 public interface DatabaseServices<T> {
-	
+
 	public T saveEntity(T entity);
+
 	public T updateEntity(T entity);
+
 	public void deleteEntity(int id);
+
 	public List<T> getEntities();
 }
